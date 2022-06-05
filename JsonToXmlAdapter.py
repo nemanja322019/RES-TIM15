1b985f067c035b18d5bf22006c8e9653753bac26
AleksandarUsljebrka <usljebrkaaleksandar@gmail.com>
2022-06-05 17:40:28 +0200

    Dodata komunikacija sa CommunicationBus-om

--- JsonToXmlAdapter.py
+++ JsonToXmlAdapter.py
@@ -2,9 +2,35 @@ import json as j
 import xml.etree.cElementTree as e
 import xmltodict
 import json
+import socket
+poruka = """{
+ "verb": "GET",
+ "noun": "/resurs/1",
+ "query": "name='pera'; type=1",
+ "fields": "id; name; surname"
+}"""
+
+IP = "127.0.0.1"
+HOST_PORT = 5006
+BUFFER_SIZE = 1024
+
+host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_socket.bind((IP, HOST_PORT))
+print("JsonXmlAdapter listening for connections..")
+host_socket.listen()
 
 
-def jsonToXml(self,request):
+
+conn, addr = host_socket.accept()
+print ('Connection address:', addr)
+
+while True:
+    primljeno = conn.recv(BUFFER_SIZE).decode()
+    if not primljeno: break
+    print ("Received data from client:", primljeno)
+    zahtev = primljeno
+
+def jsonToXml(request):
     d = json.loads(request)
     print(d)
 
@@ -21,12 +47,10 @@ def jsonToXml(self,request):
 
     print(xmlstr)
 
-    return xmlstr
 
 def xmlToJson(self,s):
-    # root = e.fromstring(s)
+    #root = e.fromstring(s)
     data_dict = xmltodict.parse(s)
     json_data = json.dumps(data_dict)
     print(json_data)
 
-    return json_data
