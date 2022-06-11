88838f6999a963e87c1c8f5646cd0431a4f6cd84
Aca0000 <markotosic070@gmail.com>
2022-06-11 21:23:45 +0200

    Prosirena provera

--- CommunicationBus.py
+++ CommunicationBus.py
@@ -1,14 +1,21 @@
 import socket
 import json
-from re import split
+import re
 
 verb_format = ["GET", "POST", "PATCH","DELETE"]
-noun_format = ["resurs","resurs_type"]
-query_fields = ["id","name","surname","type",""]
+noun_format = ["/resurs/1","/resurs/2","/resurs/3","/resurs/4"]
+
+query_fields1 = ["id","name","surname","description","type",""]
+query_fields2 = ["id","naziv",""]
+query_fields3 = ["id","idFirstUser","idSecondUser","type",""]
+query_fields4 = ["id","naziv",""]
+
+
 
 IP = "127.0.0.1"
 HOST_PORT = 5005
 SERVICE_PORT_XMLADAPTER = 5006
+SERVICE_PORT_DATABASE_ADAPTER = 5007
 BUFFER_SIZE = 1024
 
 host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,45 +24,104 @@ print("CommunicationBus listening for connections..")
 host_socket.listen()
 conn, addr = host_socket.accept()
 print ('Connection address:', addr)
+
 xml_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 xml_client_socket.connect((IP, SERVICE_PORT_XMLADAPTER))
 
-def ProveraQuery():
-    query_statements=split("; ",zahtev_dict["query"])
-    i=0
-    query_statement=[]
+sql_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sql_client_socket.connect((IP, SERVICE_PORT_DATABASE_ADAPTER))
 
-    while i<len(query_statements):
-        query_statement.append(split("=",query_statements[i]))
-        i+=1
-    for field in query_statement:
-        for j in range(len(field)):
-            if(j%2==0 and (field[j] not in query_fields)):
-                return False
-            elif (field[j]=="name" or field[j]=="surname") and "'" not in field[j+1]:
-                return False
-            elif (field[j]=="id" or field[j]=="type") and not field[j+1].isdigit():
-                return False
-    return True
+def ProveraVerb():
+    if "verb" not in zahtev_dict:
+        return False
+    return zahtev_dict["verb"] in verb_format
+
 def ProveraNoun():
-    noun = split("/",zahtev_dict["noun"])
-    noun.remove('')
-    if noun[0] not in noun_format:
+    if "noun" not in zahtev_dict:
         return False
-    if not noun[1].isdigit():
+    return zahtev_dict["noun"] in noun_format
+
+def ProveraQuery():
+    if "query" not in zahtev_dict:
         return False
-    return True
-def ProveraFormata():
-        
-    fields=split("; ",zahtev_dict["fields"])
+    noun = zahtev_dict["noun"]
     
-    if zahtev_dict["verb"] not in verb_format:
+    if noun[-1] == '1':
+        query_fields = query_fields1
+    if noun[-1] == '2':
+        query_fields = query_fields2
+    if noun[-1] == '3':
+        query_fields = query_fields3
+    if noun[-1] == '4':
+        query_fields = query_fields4
+
+    query_statement = re.split("=|; ",zahtev_dict["query"])
+    for field in query_statement:
+        for j in range(len(query_statement)):
+            if(j%2==0 and (query_statement[j] not in query_fields)):
+                return False
+            
+    return True
+
+def ProveraFields():
+    if "fields" not in zahtev_dict:
         return False
-    if  not(ProveraNoun() and ProveraQuery()):
+    noun = zahtev_dict["noun"]
+    
+    if noun[-1] == '1':
+        fields = query_fields1
+    if noun[-1] == '2':
+        fields = query_fields2
+    if noun[-1] == '3':
+        fields = query_fields3
+    if noun[-1] == '4':
+        fields = query_fields4
+
+    if zahtev_dict["verb"] == "PATCH":
+        field_statement = re.split("=|; ",zahtev_dict["fields"])
+        for field in field_statement:
+            for j in range(len(field_statement)):    
+                if(j%2==0 and (field_statement[j] not in fields)):
+                    return False
+    else:
+        field_statement = re.split("; ",zahtev_dict["fields"])
+        for field in field_statement:
+            if(field not in fields):
+                return False
+    
+    return True
+
+
+def ProveraFormata():
+    if not (ProveraVerb() and ProveraNoun()):
         return False
-    for f in fields:
-        if f not in query_fields:
+    
+    # Provera za GET
+    if zahtev_dict["verb"] == "GET":
+        if "query" in zahtev_dict:
+            if not ProveraQuery():
+                return False
+        if "fields" in zahtev_dict:
+            if not ProveraFields():
+                return False
+
+    # Provera za DELETE
+    if zahtev_dict["verb"] == "DELETE":
+        if not ProveraQuery():
             return False
+    
+    # Provera za POST
+    if zahtev_dict["verb"] == "POST":
+        if not ProveraQuery():
+            return False
+    
+    # Provera za PATCH
+    if zahtev_dict["verb"] == "PATCH":
+        if not ProveraQuery():
+            return False
+        if not ProveraFields():
+            return False
+    
     return True
         
     
@@ -64,14 +130,24 @@ while True:
     if not primljeno: break
     print ("Received data from webclient:", primljeno)
     zahtev = primljeno
-    zahtev_dict=json.loads(zahtev)
+    
+    try:
+        zahtev_dict=json.loads(zahtev)
+    except:
+        conn.send("BAD FORMAT".encode())
+        continue
+    
     provera=ProveraFormata()
     if provera == True:
         xml_client_socket.send(zahtev.encode())
-        XMLzahtev = xml_client_socket.recv(BUFFER_SIZE)
-        print(XMLzahtev)
+        XMLzahtev = xml_client_socket.recv(BUFFER_SIZE).decode()
+        sql_client_socket.send(XMLzahtev.encode())
+        odgovor = sql_client_socket.recv(BUFFER_SIZE).decode()
+        xml_client_socket.send(odgovor.encode())
+        odgovor = xml_client_socket.recv(BUFFER_SIZE).decode()
+        conn.sendall(odgovor.encode())
     else:
-        conn.send(provera.encode())
+        conn.send('{"odgovor": {"status": "BAD_FORMAT", "statuscode": "5000", "payload": "B A D   F O R M A T"}}'.encode())
 
 
 
