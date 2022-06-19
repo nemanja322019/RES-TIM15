3c9a9c24f202f00274413f52dd90cbbf20892aeb
OgnjenPR29 <sekulic.pr29.2019@uns.ac.rs>
2022-06-19 18:31:31 +0200

    Uradjeni testovi za konvertovanje odgovora iz baze u XML

--- XmlDataBaseAdapter.py
+++ XmlDataBaseAdapter.py
@@ -2,242 +2,230 @@ from select import select
 import socket
 import xml.etree.ElementTree as ET
 
+IP = "127.0.0.1"
+HOST_PORT = 5007
+SERVICE_PORT = 5008
+BUFFER_SIZE = 2048
 
-class XmlDataBaseAdapter:
+host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_socket.bind((IP, HOST_PORT))
+print("XmlDataBaseAdapter listening for connections..")
 
-    def __init__(self, zahtev):
-       self.zahtev = zahtev  
+# na osnovu zahteva(GET,POST,DELET ili PATCH) bira koju metodu poziva
+def to_sql(zahtev):
+    verb = zahtev.split("verb")
+    verb = verb[1][1:-2]
+    get = "GET"
+    post = "POST"
+    delete = "DELETE"       
 
-    # na osnovu zahteva(GET,POST,DELET ili PATCH) bira koju metodu poziva
-    def to_sql(self):
-        verb = zahtev.split("verb")
-        verb = verb[1][1:-2]
-        get = "GET"
-        post = "POST"
-        delete = "DELETE"
-        patch = "PATCH"        
-    
-        if verb in get:
-            return self.method_get_to_sql()
-        elif verb in delete:
-            return self.method_delete_to_sql()
-        elif verb in post:
-            return self.method_post_to_sql()
-        elif verb in patch:
-            return self.method_patch_to_sql()
-        else:
-            print("NE POSTOJI")
-    
-    zahtev =""
-   
-    #vraca ceo query u obliku: name='pera' AND type=1....
-    def get_query(self):
-        if("query" in zahtev):
-            query = zahtev.split("query")
-            query = query[1][1:len(query[1])-2]
-            query = query.replace(";"," AND ")
-            return query
-        else:
-            return False
-   
-    #vraca samo vredosti iz query-ja('pera' 1 ...)
-    def get_values_from_query(self):
+    if verb in get:
+        return method_get_to_sql(zahtev)
+    elif verb in delete:
+        return  method_delete_to_sql(zahtev)
+    elif verb in post:
+        return  method_post_to_sql(zahtev)
+    else:
+        return method_patch_to_sql(zahtev)
+
+zahtev =""
+
+#vraca ceo query u obliku: name='pera' AND type=1....
+def get_query(zahtev):
+    if("query" in zahtev):
         query = zahtev.split("query")
         query = query[1][1:len(query[1])-2]
-        value = query.split(";")
-        
-        values = list()
-        for i in range(len(value)):
-            temp = value[i].split("=")
-            values.append(temp[1])
-        
-        
-        return values
-   
-    def get_names_from_query(self):
-        query = zahtev.split("query")
-        query = query[1][1:len(query[1])-2]
-        value = query.split(";")
-        
-        values = list()
-        for i in range(len(value)):
-            temp = value[i].split("=")
-            values.append(temp[0])
-        
+        query = query.replace(";"," AND ")
+        return query
+    else:
+        return False
 
-        return values
-    #vraca polja u obliku: id, name, surname....
-    def get_fields(self):
-        if("fields" in zahtev):
-            fields = zahtev.split("fields")
-            fields = fields[1][1:len(fields[1])-2]
-            fields = fields.replace(";",", ")
-            return fields
-   
-    #sluzi samo za PATCH jer vraca polja sa vrednostima za upload(name='pera', type=1...)
-    def get_fields_with_values(self):
-        if("fields" in zahtev):
-            fields = zahtev.split("fields")
-            fields = fields[1][1:len(fields[1])-2]
-            fields = fields.replace(";",", ")
-            return fields
-        else:
-            return False
-
-    #iz zahteva dobavlja broj tabele
-    def get_number_table(self):
-        if("noun" in zahtev):
-            noun = zahtev.split("noun")
-            noun = noun[1].split("/")
-            noun = noun[2][:-1]
-            return int(noun)
+#vraca samo vredosti iz query-ja('pera' 1 ...)
+def get_values_from_query(zahtev):
+    query = zahtev.split("query")
+    query = query[1][1:len(query[1])-2]
+    value = query.split(";")
     
-    #na osnovu broja tabele vraca naziv tabele
-    def get_string_table(self, number):
-        switcher = {
-            1:"users",
-            2:"user_type", 
-            3:"relations",
-            4:"relation_type"
-        }
-        return switcher.get(number,"n")
+    values = list()
+    for i in range(len(value)):
+        temp = value[i].split("=")
+        values.append(temp[1])
     
-    #pretvara GET zahtev u sql upit
-    def method_get_to_sql(self):
-        sqlreq = "select "
-        if(self.get_fields()):
-            sqlreq += self.get_fields()+" from "
-        else:
-            sqlreq += "* from "
-        sqlreq += self.get_string_table(self.get_number_table())
-        if(self.get_query()):
-            sqlreq += " where "+self.get_query()
-        else:
-            return sqlreq
-        return sqlreq
-
-    #pretvara DELETE zahtev u sql upit
-    def method_delete_to_sql(self):
-        sqlreq = "DELETE from "+self.get_string_table(self.get_number_table())
-        if(self.get_query()):
-            sqlreq += " where "+ self.get_query()
-        else:
-            return sqlreq
-        return sqlreq
-
-    #pretvara POST zahtev u sql upit
-    def method_post_to_sql(self):
-        cnt = 0
-        sqlreq = "INSERT INTO " +self.get_string_table(self.get_number_table())
-        
-        sqlreq += "("
-        for value in self.get_names_from_query():
-            sqlreq+=value
-            cnt +=1
-            if cnt == len(self.get_names_from_query()):
-                break
-            sqlreq += ", "
-        sqlreq += ") VALUES("
-        
-        cnt = 0
-        for value in self.get_values_from_query():
-            cnt +=1
-            sqlreq += value
-            if cnt == len(self.get_values_from_query()):
-                break
-            sqlreq += ", "
-        sqlreq += ")"
-        return sqlreq
     
-    #pretvara PATCH zahtev u sql upit
-    def method_patch_to_sql(self):
-        sqlreq = "UPDATE "+self.get_string_table(self.get_number_table())+ " SET "
-        sqlreq += self.get_fields_with_values()
-        if self.get_query():
-            sqlreq += " WHERE " + self.get_query()
+    return values
+
+def get_names_from_query(zahtev):
+    query = zahtev.split("query")
+    query = query[1][1:len(query[1])-2]
+    value = query.split(";")
+    
+    values = list()
+    for i in range(len(value)):
+        temp = value[i].split("=")
+        values.append(temp[0])
+    
+
+    return values
+#vraca polja u obliku: id, name, surname....
+def get_fields(zahtev):
+    if("fields" in zahtev):
+        fields = zahtev.split("fields")
+        fields = fields[1][1:len(fields[1])-2]
+        fields = fields.replace(";",",")
+        return fields
+
+#sluzi samo za PATCH jer vraca polja sa vrednostima za upload(name='pera', type=1...)
+def get_fields_with_values(zahtev):
+    fields = zahtev.split("fields")
+    fields = fields[1][1:len(fields[1])-2]
+    fields = fields.replace(";",",")
+    return fields
+
+#iz zahteva dobavlja broj tabele
+def get_number_table(zahtev):
+    if("noun" in zahtev):
+        noun = zahtev.split("noun")
+        noun = noun[1].split("/")
+        noun = noun[2][:-1]
+        return int(noun)
+
+#na osnovu broja tabele vraca naziv tabele
+def get_string_table(number):
+    switcher = {
+        1:"users",
+        2:"user_type", 
+        3:"relations",
+        4:"relation_type"
+    }
+    return switcher.get(number,"n")
+
+#pretvara GET zahtev u sql upit
+def method_get_to_sql(zahtev):
+    sqlreq = "SELECT "
+    if(get_fields(zahtev)):
+        sqlreq +=  get_fields(zahtev)+" FROM "
+    else:
+        sqlreq += "* FROM "
+    sqlreq +=  get_string_table(get_number_table(zahtev))
+    if(get_query(zahtev)):
+        sqlreq += " WHERE "+ get_query(zahtev)
+    else:
         return sqlreq
+    return sqlreq
 
-    def to_xml(self, query, result):
+#pretvara DELETE zahtev u sql upit
+def method_delete_to_sql(zahtev):
+    sqlreq = "DELETE FROM "+ get_string_table(get_number_table(zahtev))
+    if(get_query(zahtev)):
+        sqlreq += " WHERE "+  get_query(zahtev)
+    else:
+        return sqlreq
+    return sqlreq
 
-        xmlodgrejected = "<odgovor><status>REJECTED</status><statuscode>3000</statuscode><payload>"
-        xmlodgrejected = xmlodgrejected  + result + "</payload></odgovor> "
+#pretvara POST zahtev u sql upit
+def method_post_to_sql(zahtev):
+    cnt = 0
+    sqlreq = "INSERT INTO " + get_string_table(get_number_table(zahtev))
+    
+    sqlreq += "("
+    for value in  get_names_from_query(zahtev):
+        sqlreq+=value
+        cnt +=1
+        if cnt == len(get_names_from_query(zahtev)):
+            break
+        sqlreq += ", "
+    sqlreq += ") VALUES("
+    
+    cnt = 0
+    for value in  get_values_from_query(zahtev):
+        cnt +=1
+        sqlreq += value
+        if cnt == len(get_values_from_query(zahtev)):
+            break
+        sqlreq += ", "
+    sqlreq += ")"
+    return sqlreq
 
-        xmlodgbadformat = "<odgovor><status>BAD_FORMAT</status><statuscode>5000</statuscode><payload>"
-        xmlodgbadformat = xmlodgbadformat  + result + "</payload></odgovor>"
+#pretvara PATCH zahtev u sql upit
+def method_patch_to_sql(zahtev):
+    sqlreq = "UPDATE "+ get_string_table(get_number_table(zahtev))+ " SET "
+    sqlreq +=  get_fields_with_values(zahtev)
+    if  get_query(zahtev):
+        sqlreq += " WHERE " +  get_query(zahtev)
+    return sqlreq
 
-        if result == "R E J E C T E D":
-            return xmlodgrejected
-        if result == "B A D   F O R M A T":
-            return xmlodgbadformat
+def to_xml(query, result):
+    xmlodgrejected = "<odgovor><status>REJECTED</status><statuscode>3000</statuscode><payload>"
+    xmlodgrejected = xmlodgrejected  + result + "</payload></odgovor>"
 
-        xmlodgsuccess = "<odgovor><status>SUCCESS</status><statuscode>2000</statuscode>"
+    xmlodgbadformat = "<odgovor><status>BAD_FORMAT</status><statuscode>5000</statuscode><payload>"
+    xmlodgbadformat = xmlodgbadformat  + result + "</payload></odgovor>"
 
-        if "select" in query:
-            if result == "EMPTY":
-                pyl="NO SUCH RESOURCE"
-            else:
-                odg = result.split(" ")  # od stringa koji je dosao iz repository se pravi lista
-                pyl = self.to_payload_select(odg, query)  # ovde se dobija payload
+    if result == "R E J E C T E D":
+        return xmlodgrejected
+    if result == "B A D   F O R M A T":
+        return xmlodgbadformat
 
-        elif "DELETE" in query:
-            pyl = "SUCCESSFULLY DELETED"
+    xmlodgsuccess = "<odgovor><status>SUCCESS</status><statuscode>2000</statuscode>"
 
-        elif "INSERT INTO" in query:
-            pyl = "SUCCESSFULLY INSERTED"
+    if "SELECT" in query:
+        if result == "EMPTY":
+            pyl="NO SUCH RESOURCE"
+        else:
+            odg = result.split(" ")  # od stringa koji je dosao iz repository se pravi lista
+            pyl = to_payload_select(odg, query)  # ovde se dobija payload
 
-        elif "UPDATE" in query:
-            pyl = "SUCCESSFULLY UPDATED"
+    elif "DELETE" in query:
+        pyl = "SUCCESSFULLY DELETED"
 
-        payload = "<payload>"+pyl+"</payload>"
+    elif "INSERT INTO" in query:
+        pyl = "SUCCESSFULLY INSERTED"
 
-        rez = xmlodgsuccess + payload + "</odgovor>"
+    elif "UPDATE" in query:
+        pyl = "SUCCESSFULLY UPDATED"
 
-        return rez
+    payload = "<payload>"+pyl+"</payload>"
 
-    def to_payload_select(self, result, query):
+    rez = xmlodgsuccess + payload + "</odgovor>"
 
-        payload = ""
+    return rez
 
-        start = query.find('select') + 7
-        end = query.find(' from', start)
-        pom = query[start:end]
+def to_payload_select(result, query):
 
-        if "*" in pom:
-            if "from users" in query:
-                pom = "id,  name,  surname,  opis,  type"
-            elif "from user_type" in query:
-                pom = "id,  naziv"
-            elif "from relations" in query:
-                pom = "id,  first_user_id,  second_user_id,  tip_id"
-            elif "from relation_type" in query:
-                pom = "id,  naziv"
+    payload = ""
 
-        fields = pom.split(","+"  ") #niz polja koja se kupe iz baze
+    start = query.find('SELECT') + 7
+    end = query.find(' FROM', start)
+    pom = query[start:end]
 
-        duzina = len(fields)#koliko ima clanova niz fielda
-        j = 0
+    if "*" in pom:
+        if "FROM users" in query:
+            pom = "id, name, surname, description, type"
+        elif "FROM user_type" in query:
+            pom = "id, title"
+        elif "FROM relations" in query:
+            pom = "id, idFirstUser, idSecondUser, type"
+        elif "FROM relation_type" in query:
+            pom = "id, title"
 
-        #u ovim petljama se pravi payload tj xml odgovor iz baze
-        while j<len(result):
-            i=0
-            payload = payload + '<resurs>'
-            while i < duzina:
-                payload = payload + '<' + fields[i] + '>' + result[j] + '</' + fields[i] + '>'
-                j = j+1
-                i = i+1
-            payload = payload + "</resurs>"
-        return payload
+    fields = pom.split(","+" ") #niz polja koja se kupe iz baze
+
+    duzina = len(fields)#koliko ima clanova niz fielda
+    j = 0
+
+    #u ovim petljama se pravi payload tj xml odgovor iz baze
+    while j<len(result):
+        i=0
+        payload = payload + '<resurs>'
+        while i < duzina:
+            payload = payload + '<' + fields[i] + '>' + result[j] + '</' + fields[i] + '>'
+            j = j+1
+            i = i+1
+        payload = payload + "</resurs>"
+    return payload
 
 if __name__ == "__main__":
 
-    IP = "127.0.0.1"
-    HOST_PORT = 5007
-    SERVICE_PORT = 5008
-    BUFFER_SIZE = 1024
-
-    host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_socket.bind((IP, HOST_PORT))
-    print("XmlDataBaseAdapter listening for connections..")
     host_socket.listen()
 
     conn, addr = host_socket.accept()
@@ -251,13 +239,9 @@ if __name__ == "__main__":
         if not primljeno: break
         print ("Received data from client:", primljeno)
         zahtev = primljeno
-        db1 = XmlDataBaseAdapter(zahtev)
-        zahtev = db1.to_sql()
+        
+        zahtev = to_sql(zahtev)
         repository_client_socket.send(zahtev.encode())
         odgovor = repository_client_socket.recv(BUFFER_SIZE).decode()
-        conn.sendall(db1.to_xml(zahtev,odgovor).encode())
+        conn.sendall(to_xml(zahtev,odgovor).encode())
         print(odgovor)
-
-
-    
-
--- testovi/test_XmlDataBaseAdapter.py
+++ testovi/test_XmlDataBaseAdapter.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append("..")
 import unittest
 from XmlDataBaseAdapter import to_sql
+from XmlDataBaseAdapter import to_xml
+from XmlDataBaseAdapter import to_payload_select
 
 
 class test_xml_data_base_adapter(unittest.TestCase):
@@ -30,6 +32,112 @@ class test_xml_data_base_adapter(unittest.TestCase):
     def test_delete_all(self):
         zahtev1="<request><verb>DELETE</verb><noun>/resurs/1</noun></request>"
         self.assertEqual(to_sql(zahtev1) , "DELETE FROM users")
-    
+
+    def test_to_xml_rej(self):
+
+        ispis = "<odgovor><status>REJECTED</status><statuscode>3000</statuscode><payload>R E J E C T E D</payload></odgovor>"
+        
+
+        zahtev = "SELECT * FROM nepostojeca"
+        odgovor = "R E J E C T E D"
+        
+        self.assertEqual(to_xml(zahtev, odgovor), ispis)
+        
+    def test_to_xml_bf(self):
+
+        ispis = "<odgovor><status>BAD_FORMAT</status><statuscode>5000</statuscode><payload>"
+        ispis = ispis + "B A D   F O R M A T</payload></odgovor>"
+
+        zahtev = "SELECT FROM users"
+        odgovor = "B A D   F O R M A T"
+        self.assertEqual(to_xml(zahtev, odgovor), ispis)
+
+    def test_to_xml_succ(self):
+
+        ispis = "<odgovor><status>SUCCESS</status><statuscode>2000</statuscode><payload>"
+        ispis = ispis + "<resurs><id>1</id><name>pera</name><surname>peric</surname></resurs>"
+        ispis = ispis + "<resurs><id>2</id><name>pera</name><surname>mikic</surname></resurs></payload></odgovor>"
+
+        zahtev = "SELECT id, name, surname FROM users WHERE name='pera'"
+        odgovor = "1 pera peric 2 pera mikic"
+        self.assertEqual(to_xml(zahtev, odgovor), ispis)
+
+    def test_to_xml_succ_del(self):
+
+        ispis = "<odgovor><status>SUCCESS</status><statuscode>2000</statuscode><payload>"
+        ispis = ispis + "SUCCESSFULLY DELETED</payload></odgovor>"
+
+        zahtev = "DELETE FROM users WHERE name='pera' AND type=1"
+        odgovor = "EMPTY"
+        self.assertEqual(to_xml(zahtev, odgovor), ispis)
+
+    def test_to_xml_succ_up(self):
+
+        ispis = "<odgovor><status>SUCCESS</status><statuscode>2000</statuscode><payload>"
+        ispis = ispis + "SUCCESSFULLY UPDATED</payload></odgovor>"
+
+        zahtev = "UPDATE users SET id=2, name='zika', surname='maric' WHERE name='pera' AND type=1"
+        odgovor = "EMPTY"
+        self.assertEqual(to_xml(zahtev, odgovor), ispis)
+
+    def test_to_xml_succ_add(self):
+        ispis = "<odgovor><status>SUCCESS</status><statuscode>2000</statuscode><payload>"
+        ispis = ispis + "SUCCESSFULLY INSERTED</payload></odgovor>"
+
+        zahtev = "INSERT INTO users(name, type) VALUES('pera', 1)"
+        odgovor = "EMPTY"
+        self.assertEqual(to_xml(zahtev, odgovor), ispis)
+
+    def test_to_xml_no_select(self):
+        ispis = "<odgovor><status>SUCCESS</status><statuscode>2000</statuscode><payload>"
+        ispis = ispis + "NO SUCH RESOURCE</payload></odgovor>"
+
+        zahtev = "SELECT id, name, surname FROM users WHERE name='pera'"
+        odgovor = "EMPTY"
+        self.assertEqual(to_xml(zahtev, odgovor), ispis)
+
+    def test_to_payload_select_all1(self):
+
+        ispis = "<resurs><id>1</id><name>pera</name><surname>peric</surname><description>opis</description>"
+        ispis= ispis + "<type>1</type></resurs>"
+
+        zahtev = "SELECT * FROM users WHERE name='pera'"
+        odgovor =  ["1", "pera", "peric", "opis", "1"]
+        self.assertEqual(to_payload_select(odgovor, zahtev), ispis)
+
+    def test_to_payload_select_all2(self):
+
+        ispis = "<resurs><id>1</id><title>musko</title></resurs>"
+
+        zahtev = "SELECT * FROM user_type WHERE id=1"
+        odgovor = ["1", "musko"]
+        self.assertEqual(to_payload_select(odgovor, zahtev), ispis)
+
+    def test_to_payload_select_all3(self):
+
+        ispis = "<resurs><id>1</id><idFirstUser>2</idFirstUser><idSecondUser>1</idSecondUser><type>3</type></resurs>"
+
+        zahtev = "SELECT * FROM relations WHERE id=1"
+        odgovor = ["1", "2", "1", "3"]
+        self.assertEqual(to_payload_select(odgovor, zahtev), ispis)
+
+    def test_to_payload_select_all4(self):
+
+        ispis = "<resurs><id>1</id><title>rodbina</title></resurs>"
+
+        zahtev = "SELECT * FROM relation_type WHERE id=2"
+        odgovor = ["1", "rodbina"]
+        self.assertEqual(to_payload_select(odgovor, zahtev), ispis)
+
+    def test_to_payload_select_regular(self):
+
+        zahtev = "SELECT id, name FROM users WHERE type=2"
+        odgovor = ["1","maja", "2", "ivana"]
+
+        ispis = "<resurs><id>1</id><name>maja</name></resurs><resurs><id>2</id><name>ivana</name></resurs>"
+
+        self.assertEqual(to_payload_select(odgovor, zahtev), ispis)
+
+
 if __name__ == "__main__":
     unittest.main()
